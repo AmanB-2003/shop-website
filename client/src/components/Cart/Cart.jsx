28b7ed4fe243f37f16105d50f0aa67ae95bfbec9
AmanB-2003 <90694309+AmanB-2003@users.noreply.github.com>
2023-03-10 19:48:44 +0530

    stripe payment integration

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,24 +3,26 @@ import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import "./Cart.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { removeItem, resetCart } from "../../redux/cartReducer";
+import {loadStripe} from '@stripe/stripe-js';
+import makeRequests from "../../makeRequests";
 
 const Cart = ()=> {
     //find products from redux state
     const products = useSelector(state=>state.cart.products);
     const dispatch = useDispatch();
 
-    const data = [
-        {
-            id: 1,
-            img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            img2: "https://images.pexels.com/photos/2530790/pexels-photo-2530790.jpeg",
-            title: "Long Sleeve Shirt",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum ",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-    ];
+    // const data = [
+    //     {
+    //         id: 1,
+    //         img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    //         img2: "https://images.pexels.com/photos/2530790/pexels-photo-2530790.jpeg",
+    //         title: "Long Sleeve Shirt",
+    //         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum ",
+    //         isNew: true,
+    //         oldPrice: 19,
+    //         price: 12,
+    //     },
+    // ];
 
     const totalPrice = ()=> {
         let total= 0;
@@ -28,6 +30,24 @@ const Cart = ()=> {
             total += item.quantity*item.price;
         });
         return total.toFixed(2);
+    };
+
+    const stripePromise = loadStripe('pk_test_51MibAxSHcbCO3ctinYGL4ea1SIu56WayTtN5fhyRanN50zUMoRKTLJCORBCHlua5ldYwjWyxpLfX70RvOyTqEECm007Notae4b');
+
+    const handlePayment = async ()=>{
+        try{
+            const stripe = await stripePromise;
+
+            const res = await makeRequests.post("/orders",{
+                products,
+            });
+
+            await stripe.redirectToCheckout({
+                sessionId: res.data.stripeSession.id,
+            })
+        }catch(err){
+            console.log(err);
+        }
     }
 
     return(
@@ -49,7 +69,7 @@ const Cart = ()=> {
         <span>SUBTOTAL</span>
         <span>${totalPrice()}</span>
      </div>
-     <button>PROCEED TO CHECKOUT</button>
+     <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
      <span className="reset" onClick={()=>dispatch(resetCart())}>Reset Cart</span>
     </div>
     );
